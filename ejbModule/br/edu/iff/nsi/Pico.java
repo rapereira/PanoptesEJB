924c090a24a7d546f21ec0b1ed8d7f2b94692f6c
rapereira <alvespereira.rap@gmail.com>
2011-09-26 16:43:02 -0300

    Adicionados arquivos de Persistencia do projeto PanoptesEJB

---
+++ ejbModule/br/edu/iff/nsi/Pico.java
@@ -0,0 +1,72 @@
+package br.edu.iff.nsi;
+
+import java.io.Serializable;
+import javax.persistence.*;
+
+
+/**
+ * The persistent class for the pico database table.
+ * 
+ */
+@Entity
+public class Pico implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	private String idpico;
+
+	private float amplitude;
+
+	private float fase;
+
+	private float frequencia;
+
+	//bi-directional many-to-one association to Leitura
+    @ManyToOne
+	@JoinColumn(name="idleitura")
+	private Leitura leitura;
+
+    public Pico() {
+    }
+
+	public String getIdpico() {
+		return this.idpico;
+	}
+
+	public void setIdpico(String idpico) {
+		this.idpico = idpico;
+	}
+
+	public float getAmplitude() {
+		return this.amplitude;
+	}
+
+	public void setAmplitude(float amplitude) {
+		this.amplitude = amplitude;
+	}
+
+	public float getFase() {
+		return this.fase;
+	}
+
+	public void setFase(float fase) {
+		this.fase = fase;
+	}
+
+	public float getFrequencia() {
+		return this.frequencia;
+	}
+
+	public void setFrequencia(float frequencia) {
+		this.frequencia = frequencia;
+	}
+
+	public Leitura getLeitura() {
+		return this.leitura;
+	}
+
+	public void setLeitura(Leitura leitura) {
+		this.leitura = leitura;
+	}
+	
+}
